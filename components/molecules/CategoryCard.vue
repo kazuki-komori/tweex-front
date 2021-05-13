8c801beb24ac26049b5451ba7b076714b8fb9322
kazuki-komori <komokazu.nankai@gmail.com>
2021-05-13 13:17:08 +0900

    update: category

--- components/molecules/CategoryCard.vue
+++ components/molecules/CategoryCard.vue
@@ -3,13 +3,17 @@
     class="shadow rounded bg-white border-2 hover:border-blue-400 cursor-pointer"
   >
     <div class="c-banner rounded-t bg-yellow-300 w-full" />
-    <div class="p-3 font-medium text-xl">
-      <p>hoge</p>
+    <div class="p-3">
+      <p class="text-gray-700 font-semibold text-xl pb-2">タイトル</p>
+      <p class="text-blue-400 text-lg pb-3">20 tweets</p>
+      <p class="border-t-2 border-gray-300 text-gray-400 text-lg pt-3">
+        最終更新 : 2021/2/2
+      </p>
     </div>
   </div>
 </template>
 <style scoped>
 .c-banner {
-  min-height: 20px;
+  min-height: 7px;
 }
 </style>
